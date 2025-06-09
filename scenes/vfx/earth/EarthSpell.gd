355f2c811426af02d9d332be221902e5a32d09e4
tiagobassini <tiagobassini@gmail.com>
2025-06-09 14:30:21 -0300

    earth spell 5

--- scenes/vfx/earth/EarthSpell.gd
+++ scenes/vfx/earth/EarthSpell.gd
@@ -4,13 +4,6 @@ extends Node3D
 
 @onready var spikes: Node3D = $Spikes
 
-@onready var spike_1: Node3D = $Spikes/Spike1
-@onready var spike_2: Node3D = $Spikes/Spike2
-@onready var spike_3: Node3D = $Spikes/Spike3
-@onready var spike_4: Node3D = $Spikes/Spike4
-@onready var spike_5: Node3D = $Spikes/Spike5
-@onready var spike_6: Node3D = $Spikes/Spike6
-
 var passed: bool = false
 
 
@@ -22,39 +15,31 @@ var spikes_count: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#spikes.get_children()
-	#_verify_ground_size()
-	#_set_spikes_visibility()
+	_verify_ground_size()
 	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	if not passed:
-		passed = true
-		print("start")
-		_verify_ground_size()
-		#_set_spikes_visibility()
-	pass
 
 
 
 func _verify_ground_size()->void:
-	
+	var end_ground = false
 	var sensores:Array = ground_sensor.get_children()
 	for sensor:RayCast3D in sensores :
 		await get_tree().process_frame
 		if not sensor.is_colliding():
 			print("i: ", spikes_count , " não colidiu")
-			break
+			end_ground = true
+		
+		if end_ground :
+			var spike: Node3D = spikes.get_child(spikes_count)
+			spike.hide()
+			for child in spike.get_children():
+				child.queue_free()
+		else:
+			print("i: ", spikes_count , " colidiu")
+			spikes.get_child(spikes_count).show()
+			print("spike ",spikes_count, ": ",spikes.get_child(spikes_count).name, " foi exibido")
 		spikes_count+=1
+		
 	pass
 
-
-func _set_spikes_visibility()->void:
-	
-	var i:int = 0
-	
-	while i<spikes_count:
-		spikes.get_child(i).show()
-	pass
